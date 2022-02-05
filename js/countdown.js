f68f345f16f9e5178063fec0a8c8500c2b9f3041
Andy Rosales <andy@Andys-MacBook-Pro.local>
2022-02-04 21:57:51 -0800

    fix timer

--- js/countdown.js
+++ js/countdown.js
@@ -1,7 +1,7 @@
 var countDownDate = new Date();
     // Replace with desired date 'January 17, 2019 03:24:00', example
     // countDownDate.setDate('January 17, 2019 03:24:00').getTime();
-    countDownDate.setDate('March 5, 2022 00:00:00'.get);
+    countDownDate.setDate('March 5, 2022 00:00:00'.get());
     countDownDate.getTime();
 
     // Update the count down every 1 second
